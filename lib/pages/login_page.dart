e9a2f4d1c14ba91423cb4cca55ef1a8530c9f6cc
IMU-IMRAN <sk7857772@gmail.com>
2023-09-27 22:01:28 +0530

    Day4 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,19 +1,58 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
- // const LoginPage({super.key});
+  // const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page",
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
-        ),),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "photos/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter User Name", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(),
+                      onPressed: () {
+                        print("Heyy Imrannnn");
+                      })
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
