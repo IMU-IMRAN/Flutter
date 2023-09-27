ec606cbeeb0ea004eb8463de4e7fbd14048879aa
IMU-IMRAN <sk7857772@gmail.com>
2023-09-27 23:57:46 +0530

    Day 5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   //const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
      // home: HomePage(),
        themeMode: ThemeMode.light ,
        theme: ThemeData(primarySwatch: Colors.deepPurple,
-       fontFamily: GoogleFonts.lato().fontFamily
+       fontFamily: GoogleFonts.lato().fontFamily,
       // primaryTextTheme: GoogleFonts.latoTextTheme()
        ),
+       //debugShowCheckedModeBanner: false,
        darkTheme: ThemeData(
         brightness: Brightness.dark
       // primarySwatch: Colors.red
@@ -26,8 +26,8 @@ class MyApp extends StatelessWidget {
       // initialRoute: "/home",
        routes: {
         "/":(context) => LoginPage(),
-        "/home":(context) => HomePage(),
-        "/login":(context) => LoginPage()
+        MyRoutes.homeRoute:(context) => HomePage(),
+        MyRoutes.loginRoute:(context) => LoginPage()
        },
     );
   }
