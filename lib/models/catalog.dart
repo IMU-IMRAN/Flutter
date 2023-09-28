4f3befe17d89456fe47715993b100627495dd778
IMU-IMRAN <sk7857772@gmail.com>
2023-09-29 01:11:55 +0530

    Day 11 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -9,8 +9,6 @@ class CatalogModel{
     price: 999,
     color: "#33505a",
     image: "https://images.unsplash.com/photo-1620555576588-a7dd6c04c55d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
-      //"https://media.istockphoto.com/id/1400788570/photo/realistic-mobile-phone-mockup-template.jpg?s=2048x2048&w=is&k=20&c=GDzbaWzzysd0E_Ih-1oR5Iu4hTxqirgLs5XjBZ6XLWc=",
-       //"photos/images/iphone12pro.jpg",
 
     )
 ];
